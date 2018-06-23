8a407d1104e0bbc61189515f72f6b521f29160f1
Taiza <taizarabello@gmail.com>
2018-06-23 11:32:19 -0300

    Initial commit. Basic project of Bootstrap initializar

---
+++ src/main/java/com/vanhack/api/skipthedishes/SkipthedishesApplication.java
@@ -0,0 +1,14 @@
+package com.vanhack.api.skipthedishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SkipthedishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipthedishesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vanhack/api/skipthedishes/SkipthedishesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vanhack.api.skipthedishes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SkipthedishesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
